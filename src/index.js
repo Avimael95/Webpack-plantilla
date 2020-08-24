afd0b97ba1fd32fe53b84e25932e2ba1841b1c26
Avimael <ing.avimelgr95@gmail.com>
2020-08-24 12:23:49 -0500

    Se agrega estilos a la barra

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
 //otra forma de cargar los archivos
 import './styles/main.scss';
 import './static/logo.png';
+import './static/cart.svg';
+import './static/css.png';
+import './static/estrellas.png';
+import './static/git.jpg';
+import './static/hero.jpg';
+import './static/html.png';
+import './static/icono1.png';
+import './static/icono2.png';
+import './static/icono3.png';
+import './static/Javascript.png';
+import './static/laravel.png';
+import './static/lupa.png';
+import './static/sass.jpg';
+import './static/webpack.png';
+import './static/xd.jpg';
+
 
 
