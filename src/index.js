ca9e13f1371e4da7ce9a4640ee0606aec3f76b30
Avimael <ing.avimelgr95@gmail.com>
2020-08-28 15:50:35 -0500

    Se cofigura el fontawesome

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 // require('./styles/main.scss');
 //otra forma de cargar los archivos
 import './styles/main.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 import './static/webpack.png';
 
 
